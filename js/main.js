5bc679311f7bbd013375c065f728a0ce1d5bb0eb
lukasz ciepichal <lukaszciepichal1@gmail.com>
2021-11-06 14:53:02 +0100

    dodaj-usun tekst

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let paragraf = document.getElementById('#tekst');
+    button = document.getElementById('#dodaj');
+    button = document.getElementById('#usun')
+
+ document.querySelector('#dodaj').onclick = function (){
+    tekst.textContent = 'Ala ma kota'
+}
+
+document.querySelector('#usun').onclick = function (){
+    tekst.textContent = ''
+}
+
